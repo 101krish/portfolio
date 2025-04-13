17fa06a5025e02b8644acfaf98daf96a8b8f3043
Krish Maheshwari <krishmaheshwari2003@gmail.com>
2025-04-13 20:12:37 +0530

    Initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,35 @@
+const config = {
+  information: {
+    name: "Krish Maheshwari",
+    helloText: "Hello",
+    welcomeText: "✨ Welcome to my universe",
+    title: "Software Developer 🚀 (Developing what world needs!)",
+    title3: "and coding the future 💻 ✨",
+  },
+  flipWords: ["Backend Developer", "Cloud Computing", "Crafting frameworks", "Frontend Web Developer"],
+  codeContent: {
+    code: `const profile = {
+  name: 'Krish Maheshwari',
+  title: 'MERN Developer | SDE | Problem Solver',
+  skills: [
+    'React', 'NextJS', 'Redux', 'Express',
+    'MySQL', 'MongoDB', 'Docker', 'AWS', 'TypeScript',
+    'JAVA', 'Git'
+  ],
+  hardWorker: true,
+  quickLearner: true, 
+  problemSolver: true,
+  yearsOfExperience: 1,
+  hireable: function() {
+    return (
+      this.hardWorker &&
+      this.problemSolver &&
+      this.skills.length > 5 &&
+      this.yearsOfExperience >= 0
+    );
+  }
+};`,
+  },
+};
+
+export default config;
